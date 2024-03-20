19047f8554db741c09c7b0a20f92fc0cac863300
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-19 21:55:48 -0600

    varibles y definiciones basicas

--- js/java.js
+++ js/java.js
@@ -1,1 +1,14 @@
 //javascript
+/* 
+variables = espacios en memoria, guardar funciones. 
+
+
+declarar: creando una variable
+
+inicializar: darle un valor.
+
+reinicializar: cambiar el valor de una variable.
+
+
+
+*/
