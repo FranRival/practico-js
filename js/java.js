c4d78a8faa44f3e77aa9dc66a6209b0d1a47a48a
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-19 22:08:07 -0600

    variables simples con string

--- js/java.js
+++ js/java.js
@@ -9,6 +9,29 @@ inicializar: darle un valor.
 
 reinicializar: cambiar el valor de una variable.
 
+--el operador que permite concatenar y sumar:
+
 
 
 */
+
+
+
+let suma = 2+2 
+
+let stringsuman = 'Hello' + 3 //sumar o cancatenar es la +
+
+
+//tipos de datos.
+
+const nombre = 'Leira'
+const apellido ='Maira'
+const nombreUsuario = 'LeiraMa'
+let edad
+const correoElectronico = ''
+const mayorEdad = true
+let dineroAhorrado
+let deudas 
+
+
+console.log(nombre+ ' ' +apellido)
