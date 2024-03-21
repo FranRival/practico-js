b18880127ee793a3df524e3fa7bda7fcb2d5ba5f
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-20 23:10:33 -0600

    if, else, else if, stich, cases parte.2

--- js/java.js
+++ js/java.js
@@ -3,3 +3,28 @@
 //la diferencia: 
 
 //if, else, else if, switch.
+//arrays. 
+
+//switch: evaluar caso. y por cases, cumpen con esa condicion. 
+
+
+const tipoDeSuscripcion = "Basic";
+
+switch (tipoDeSuscripcion) {
+   case "Free":
+       console.log("Solo puedes tomar los cursos gratis");
+       break;
+   case "Basic":
+       console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+       break;
+   case "Expert":
+       console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+       break;
+   case "ExpertPlus":
+       console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+       break;
+}
+
+//validando la misma condicion. 
+//el condicional if: permite validaciones distintas en cada validacion o condiconal.
+//switch: todos los cases se comparan con la misma variable o condicone que definimos en el switch.
