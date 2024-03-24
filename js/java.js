b9e978246cf5bb4513ba76efba74f5eaa4776301
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-23 23:59:44 -0600

    funciones del for con objetos

--- js/java.js
+++ js/java.js
@@ -11,3 +11,21 @@ const obj = {//el obejto ahora es una lista de elementos con nombre clave.
     nombre: '',
     edad: 3,
 }
+
+//arrays: elementos lo mismo que en todos los demas.
+//objeto: cuando los nombres de cada elemento son importantes para el algoritmo.
+
+function imprimirPrimerElementoArray (arr){
+    console.log(arr[0])
+}
+
+imprimirPrimerElementoArray(['Ana', 'Celaya']) 
+
+function imprimierElementoPorElemento (arr){
+    for (let index = 0; index < arr.length; index++) {
+        console.log(arr[index])
+    }
+}
+
+console.log('-----------------');
+imprimierElementoPorElemento(['Ana', 'Celaya'])
