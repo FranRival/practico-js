672c82c51240012d876449c15e87486b3ef4a9a0
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-23 23:48:34 -0600

    direfencias entre corchetes y parentesis cuadrados para los arrays

--- js/java.js
+++ js/java.js
@@ -1,42 +1,13 @@
-//ciclos
-//la forma de ejecutar un bloque de codigo hasta que se cumple cierta condicion 
+//arrays y objetos. 
 
-//while, for, do while.
 
-//dowhile - while
-//while: hace la validacion antes de ejecutar la pimera vez el bloque de codigo.
-//dowhile: la primera vez no pregunta, ejecuta, y luego hace la ejecucion. 
-//for: automaticamente, nos obliga a definir al principio y final de cada ejecucion de codigo. 
-//obliga a ainicializar una variable, definamos algun momento donde debe terminar, y una variable que daba cambiar. 
+//estructura de datos con javasscript.
+//arrays: lista de elementos.
+//lista: conjunto de elementos.
 
-for (let index = 0; index < 5; index++) {
-    console.log("EL valor de i es: " + index)
+const arrays = []
+
+const obj = {//el obejto ahora es una lista de elementos con nombre clave.
+    nombre: '',
+    edad: 3,
 }
-
-//ciclo infnito - for
-//la condicion para terminar de ejecutar el ciclo, nunca se cumple. y termina danando al navegador. 
-
-console.log('------------------');
-
-let index = 0
-while (index < 5) {
-     console.log("EL valor de i es: " + index)
-     index++ //asi evitamos el ciclo infinito.
-}
-
-console.log('------------------');
-
-let index1 = 10
-while (index1 >= 2) {
-     console.log("EL valor de index1 es: " + index1)
-     index1-- //asi evitamos el ciclo infinito.
-}
-
-console.log('------------------');
-
-
-let respuesta
-while (respuesta !=4) {
-    let pregunta = prompt('CUanto es 2 + 2?')
-    respuesta = pregunta
-} //ciclo infinto
