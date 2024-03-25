1fad42d3864bb82f45f21fce66de9d69f3771116
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-24 22:28:03 -0600

    tipo de suscripcion y reescribiendo el codigo para hacerlo escalable

--- js/java.js
+++ js/java.js
@@ -2,23 +2,30 @@
 
 
 
-function conseguirTipoSuscripcion (suscripcion){
-    if (suscripcion=='Free') {
-        console.log('Tomaras los cursos gratis.');
-        return
-    }if (suscripcion=='Basic') {
-        console.log('Basic');
-        return
-    }if (suscripcion=='Expert') {
-        console.log('Expert')
-        return
-    }if (suscripcion == 'Expert plus') {
-        console.log('Expert plus');
-        return
-    }else {
-        console.warn('Not exist that bitch');
-    }
+//arrays y un condcional. un solo if.
+//50+ condiconales de suscripcion. por cada suscripcion, hay 4 lineas de codigo
+//escalabilidad.
+
+const tiposDeSuscripciones = {
+    free: 'Cursos gratis',
+    basic: 'Cursos basic',
+    expert: 'Cursos expert',
+    expertPlus: 'Expert plus'
 }
 
+tiposDeSuscripciones['Free']
+
+const ejemploSuscripcion = 'Free'
+
+tiposDeSuscripciones [ejemploSuscripcion]
+
+//reescribiendo esta funcion.
+
+function conseguirTipoSuscripcion (suscripcion){
+    if (tiposDeSuscripciones[suscripcion]) {
+
+
+}
+}
 conseguirTipoSuscripcion('jj')
 
