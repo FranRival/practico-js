7bdb64363d08c7cea05d000b3d196661d72d90df
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-25 23:52:07 -0600

    se arregla cambiando el script de lugar, al interior de body

--- js/java.js
+++ js/java.js
@@ -4,9 +4,11 @@
 
 const h1 = document.querySelector('h1')
 const p = document.querySelector('p')
-const primerParrafo = document.querySelector('#primer-parrafo')
-const cssBasico = document.querySelector('.css-basico')
+const primerParrafo = document.querySelector('.primer-parrafo')
+const cssBasico = document.querySelector('#css-basico')
 const input = document.querySelector('input')
+
+console.log(h1);
 
 console.log({
     h1,
