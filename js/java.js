b4d76eb26afa21cbda8be06a0a2cd7e937cb1c6b
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-25 23:44:58 -0600

    resolviendo el error de ligar el javascript con el dom

--- js/java.js
+++ js/java.js
@@ -1,1 +1,17 @@
-//
+//leyendo html desde javascript.
+
+
+
+const h1 = document.querySelector('h1')
+const p = document.querySelector('p')
+const primerParrafo = document.querySelector('#primer-parrafo')
+const cssBasico = document.querySelector('.css-basico')
+const input = document.querySelector('input')
+
+console.log({
+    h1,
+    primerParrafo,
+    cssBasico,
+    p,
+    input 
+});
