a118c15b1b22296919f1e8653d5100c2ba4d4e3f
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-28 23:40:52 -0600

    para hacer que el html envie informacion al javascript. onclick, onchange

--- js/java.js
+++ js/java.js
@@ -1,45 +1,11 @@
-//leyendo html desde javascript.
+//interaccion con el usuario.
 
 
 
 const h1 = document.querySelector('h1')
-const p = document.querySelectorAll('p') //se;eccionar todos los parrafos. All
-const primerParrafo = document.querySelector('.primer-parrafo')
-const cssBasico = document.querySelector('#css-basico')
-const input = document.querySelector('input')
-
-console.log(h1);
-
-console.log({
-    h1,
-    primerParrafo,
-    cssBasico,
-    p,
-    input 
-});
+const input1 = document.querySelector('#calculo')
+const input2 = document.querySelector('#calculo1')
+const btn = document.querySelector('btncalcular')
 
 
-h1.innerHTML= 'PRACTICO JS HTML'
-
-
-console.log(h1.getAttribute('Pantalla'))
-console.log('-----');
-
-
-const h2 = document.querySelector('h2')
-h2.setAttribute("class","dorado")
-
-
-h2.classList.add('Fiuzha')//agregamos una nueva clase. 
-h2.classList.remove('dorado')//quitar clase
-
-input.value = 657
-
-//crear desde 0
-
-
-const imagen = document.createElement('img')
-imagen.setAttribute('src', 'https://www.findelahistoria.com/web/wp-content/uploads/2018/02/garfield.jpg')
-console.log(imagen);
-img.innerHTML = '' //AQUI, ELIMINAMOS EL :aqui va una imagen.
-img.append(imagen)//Aqui colocamos la imagen 
+//escuchar elementos desde html a js: onclick - onchance
