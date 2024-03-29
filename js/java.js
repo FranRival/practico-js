9c787e69236f5b27a56ed03e7e6c0b7285727ee0
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-28 23:53:05 -0600

    usando la funcion de numbers, datos metidos por string convertidos a numeros. y luego, modificar y aparecer nuevo texto en el html

--- js/java.js
+++ js/java.js
@@ -6,11 +6,16 @@ const h1 = document.querySelector('h1')
 const input1 = document.querySelector('#calculo')
 const input2 = document.querySelector('#calculo1')
 const btn = document.querySelector('btncalcular')
+const resultado = document.querySelector('#resultado')
 
 
 //escuchar elementos desde html a js: onclick - onchance
 
 
 function btnOnClick (){
-    console.log('Escuchando el evento de click');
+    console.log(input1.value + input2.value);//concatenacion. no suma. 
+    console.log(Number(input1.value) + parseInt(input2.value))//suma
+
+    const result = input1.value + input2.value
+    resultado.innerHTML = 'Resultado: ' + result
 }
