9b604e07b0b523e56c51553a58ce96cea814a674
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-29 23:57:14 -0600

    forumulario en html, y creando un eventlistener form. quitando el resultado y valores por defecto que usaria normalmente el form

--- js/java.js
+++ js/java.js
@@ -7,16 +7,25 @@ const input1 = document.querySelector('#calculo')
 const input2 = document.querySelector('#calculo1')
 const btn = document.querySelector('#btncalcular')
 const resultado = document.querySelector('#resultado')
+const formulario = document.querySelector('#formulario')
 
 
 
-btn.addEventListener('click', btnOnClick) //sin parentesis. 
+formulario.addEventListener('submit', sumarInputValues)
 
 
-function btnOnClick (){
+function sumarInputValues (event){
+    console.log({event})
+    event.preventDefault()//ya no se recargo la pagina.
+    //defaultPrevent: que no ejecute lo que ejecutaria por defecto.
+
+
+
     console.log(input1.value + input2.value);//concatenacion. no suma. 
     console.log(Number(input1.value) + parseInt(input2.value))//suma
 
     const result = input1.value + input2.value
     resultado.innerHTML = 'Resultado: ' + result
 }
+
+//trabajr con formularios no es escuchar eventos con el addeventlistener. 
